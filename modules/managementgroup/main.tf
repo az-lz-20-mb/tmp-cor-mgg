ad41fb3abffb953b9f6928e95b54075dc9a4176d
Wiatrowski <j.wiatrowski@accenture.com>
2025-02-03 12:14:36 +0100

    change2

--- modules/managementgroup/main.tf
+++ modules/managementgroup/main.tf
@@ -1,8 +1,6 @@
 module "management_groups" {
   source = "git::https://github.com/az-lz-20-mb/mod-cus-res-management-group"
-
   for_each = var.management_groups
-
   name                          = each.value.name
   parent_management_group_id    = each.value.parent_management_group_id
 
